23993f8e6b1ab1282b4a581e0a19cbc915bdf053
Cainã Brazil <cainabrazil@gmail.com>
2018-02-26 18:33:45 +0000

    comments

--- exercicios/1/parque.cpp
+++ exercicios/1/parque.cpp
@@ -8,17 +8,17 @@ int main() {
   cout << "Digite a duração da estada no parqueamento (em horas), seguido de \'enter\' \n";
   cin >> dur;
 
-  float pagar = 0.8;
+  float pagar = 0.8; //até uma hora
 
   if(dur <= 1){
     cout << "Valor a pagar: " << pagar;
   }
   else if(dur <= 4){
-    pagar += (dur - 1) * 1;
+    pagar += (dur - 1) * 1; //até uma hora + proporcional até a terceira hora
     cout << "Valor a pagar: " << pagar;
   }
   else if(dur > 4){
-    pagar += 2 + (dur - 3) * 1.2;
+    pagar += 2 + (dur - 3) * 1.2; //até uma hora + proporcional até a terceira hora + quarta hora em diante
     cout << "Valor a pagar: " << pagar;
   }
   return 0;
